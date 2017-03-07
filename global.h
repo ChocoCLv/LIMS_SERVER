123cd988e55c0c08a89ef77a973bdf4bdb09396a
ChocoCLv <choco_cl@163.com>
2017-03-07 21:57:53 +0800

    记录多线程实现的问题

--- global.h
+++ global.h
@@ -7,7 +7,7 @@ enum RequestType{LOGIN};
 const static int SVR_PORT = 2222;
 
 
-const static QString DB_SERVER_ADDR = "192.168.199.127";
+const static QString DB_SERVER_ADDR = "localhost";
 
 const static int DB_SERVER_PORT = 3306;
 
